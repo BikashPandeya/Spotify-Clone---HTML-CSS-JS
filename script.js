b096cf559706fbbacfeab8caf53ef7f14f15760c
BikashPandeya <bikashpandeya5@gmail.com>
2025-01-23 22:31:48 +0545

    Spotify Clone

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 console.log("Lets write some js");
 
 
-async function main() {
+async function getSongs() {
     
-    let a  = await fetch("https://http://127.0.0.1:3000/index.html")
+    let a  = await fetch("http://127.0.0.1:3000/mysongs/")
     let response = await a.text()
-    console.log(response);
-    
+    // console.log(response);
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs =[]
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith("mpeg")){
+            songs.push(element.href)   
+        }  
+    }
+   return songs;
 }
 
-
+async function main() {
+    
+    //To get the list of all the songs
+    let songs = await getSongs();
+    console.log(songs);
+    
+}
 main()
